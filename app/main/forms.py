518113bfec85be585fe66634d8cd4dfda0683eeb
tonytunde2012 <tonytunde2012@gmail.com>
2017-05-10 15:13:05 +0100

    Rendering the application Now

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,23 @@
-from . import db
+from app import db
 from flask_wtf import FlaskForm
-from wtforms import FieldList, FormField, StringField
+from wtforms import FieldList, FormField, StringField, SelectField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
-class AddressEntryForm(FlaskForm):
-    name = StringField()
+class PremiseForm(FlaskForm):
+    name = StringField('Premise Name', validators=[DataRequired(), Length(1, 64)])
+    address = StringField('Address', validators=[DataRequired()])
+
+
+class Drug(FlaskForm):
+    drugName = StringField('Drug Name', validators=[DataRequired(), Length(1, 64)])
+    drugRoute = SelectField('Route of Administration', choices=[('Uyo', 'Uyo'), ('Delta', 'Asaba')])
+    drugDose = IntegerField('Drug Dosage', validators=[DataRequired()])
+    drugStrenght = IntegerField('Dosage Available', validators=[DataRequired()])
+    drugDuration = StringField('Duration of Use', validators=[DataRequired()])
+    submit = SubmitField('Dispensed')
 
 
 class AddressesForm(FlaskForm):
     """A form for one or more addresses"""
-    addresses = FieldList(FormField(AddressEntryForm), min_entries=2)
+    drug = FieldList(FormField(Drug), min_entries=2)
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,13 @@
 from . import main
+from flask import render_template
+from .forms import AddressesForm
+
 
 @main.route('/')
 @main.route('/index')
 def index():
-    return 'Hello World!'
+    drug = [{"drugName": "Amoxicillin"}]
+    user_addresses = [{"name": "First Address"},
+                      {"name": "Second Address"}]
+    form = AddressesForm()
+    return render_template("firstform.html", form=form)
